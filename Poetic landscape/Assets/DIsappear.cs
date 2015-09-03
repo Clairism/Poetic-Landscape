24242b2b82223d4cbe896300621d563deb4cf8b4
Claire Hu <hum085@newschool.edu>
2015-09-03 17:58:06 -0400

    With Triggers

---
+++ Poetic landscape/Assets/DIsappear.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DIsappear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	void OnTriggerEnter(Collider other) {
+		gameObject.GetComponent<MeshRenderer>().enabled = false;
+	}
+
+	void OnTriggerExit(Collider other) {
+		gameObject.GetComponent<MeshRenderer>().enabled = true;
+	}
+	
+
+}
---
+++ Poetic landscape/Assets/lighttrigger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lighttrigger : MonoBehaviour {
+	private GameObject bbc;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//when a player walks through the trigger
+	void OnTriggerEnter(Collider other) {
+	//get the light of this game object, and turn it off
+	gameObject.GetComponent<Light>().enabled = false;
+
+	//find the dead person and make it disappear
+		bbc = GameObject.Find ("Person");
+		bbc.SetActive(false);
+	}
+
+	void OnTriggerExit(Collider other) {
+		gameObject.GetComponent<Light>().enabled = true;
+
+		bbc.SetActive(true);
+
+	}
+
+}
